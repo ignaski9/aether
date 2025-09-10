84b1d6618f57c7c568d13e32b9bbd31987689dee
ignaski <ignaski@icloud.com>
2025-09-10 18:50:31 -0200

    switch to CMake

---
+++ src/head/particle.hpp
@@ -0,0 +1,89 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include<math.h>
+
+constexpr float PARTICLE_SIZE = 20.0f;
+constexpr int PARTICLE_SPEED = 0;
+constexpr int PARTICLE_MASS = 20;
+constexpr int PARTICLE_AMOUNT = 3;
+constexpr sf::Vector2 WINDOW_SIZE = { 1920, 1080 };
+static const float gravity = 9.81f;
+static const float GRAVITION_CONST = 6.7 * 10;
+
+//= 6.6743* 10e-11f;
+
+
+
+
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+
+
+struct Particle {
+
+	static const int mass = PARTICLE_MASS;
+	sf::Vector2f velocity, acceleration, position, position0;
+
+	sf::CircleShape shape;
+
+	void ParticleInit(sf::Vector2f pos, sf::CircleShape s) {
+		velocity = { 0.0f, 0.0f };
+		acceleration = { 0.0f, gravity };
+		position = pos;
+		position0 = pos;
+		shape = s;
+	};
+};
+
+struct ParticleSystem {
+	static std::vector<Particle> Particles;
+
+
+	sf::Vector2f CalculateDistance(Particle p1, Particle p2) {
+		float deltaY = p1.position.y - p2.position.y;
+		float deltaX = p1.position.x - p2.position.x;
+
+		return{ deltaX, deltaY };
+	}
+	float CalculateDistanceY(Particle p1, Particle p2) {
+		return p1.position.y - p2.position.y;
+	}
+	float CalculateDistanceX(Particle p1, Particle p2) {
+		return p1.position.x - p2.position.x;
+	}
+
+	void ApplyForces(float dt) {
+		for (auto& particle : Particles) {
+			for (int i = 0; i < Particles.size(); i++) {
+				
+
+				sf::Vector2f distance = CalculateDistance(particle, Particles[i]);
+
+				if (distance.x == 0 && distance.y == 0) continue;
+				float r = sqrt(distance.x * distance.x + distance.y * distance.y);
+				float softR = sqrt(r * r + 1.0f);
+				float scale = 1 / (softR * softR * softR);
+				particle.acceleration += -1 * GRAVITION_CONST * Particles[i].mass * distance * scale;
+
+				
+			}
+			particle.velocity += particle.acceleration * dt;
+			particle.position += particle.velocity * dt;
+			particle.shape.setPosition(particle.position);
+
+		}
+	};
+	void SetPos0() {
+		for (auto& particle : Particles) {
+			particle.position = particle.position0;
+		}
+
+	};
+	
+
+
+
+
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include "head/particle.hpp"
+
+std::vector<Particle> ParticleSystem::Particles;
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode({WINDOW_SIZE.x, WINDOW_SIZE.y}), "SFML works!", sf::State::Fullscreen);
+    window.setFramerateLimit(240);
+
+
+    sf::CircleShape default_shape(PARTICLE_SIZE);
+    default_shape.setFillColor(sf::Color::Red);
+
+    ParticleSystem system;
+    for (int i = 0; i < PARTICLE_AMOUNT; i++) {
+        Particle particle;
+        sf::Vector2f pos = { 100.0f + i * 200.0f, 100.0f + 50.0f*(i%2)};
+        particle.ParticleInit(pos, default_shape);
+
+
+        system.Particles.push_back(particle);
+    }
+
+
+
 
     while (window.isOpen())
     {
@@ -12,10 +32,44 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+            
+            if (const auto* keyboardButtonPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyboardButtonPressed->scancode == sf::Keyboard::Scan::Escape)
+                    window.close();
+
+
+            }
+
+
+            if(const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>())
+            {
+                if (mouseButtonPressed->button == sf::Mouse::Button::Right)
+                {
+                    system.SetPos0();
+                }
+                else  if (mouseButtonPressed->button == sf::Mouse::Button::Left)
+                {
+                    Particle particle;
+                    sf::Vector2i p = sf::Mouse::getPosition(window);
+                    sf::Vector2f pos = { p.x * 1.0f, p.y * 1.0f };
+                    particle.ParticleInit(pos, default_shape);
+
+                    system.Particles.push_back(particle);
+                }
+            }
+    
         }
+       
+       
+
+        constexpr float dt = 0.001;
+        system.ApplyForces(dt);
 
         window.clear();
-        window.draw(shape);
+        for (auto& particle : system.Particles) {
+            window.draw(particle.shape);
+        }
         window.display();
     }
 }
